878796fd217249f09bb4dfa9bc21a25310b3afb8
geragt280 <ben.ten550@gmail.com>
2024-08-23 19:33:57 +0500

    vercel issue fixed and file download from ftp done

--- src/methods/vendors/alphabroder/index.ts
+++ src/methods/vendors/alphabroder/index.ts
@@ -5,15 +5,25 @@ const router = Router();
 
 router.get("/download", (req, res) => {
   const ftpClient = new ftp();
-  const fileName = req.query.file?.toString(); // Assuming the file name is passed as a query parameter
+  const filePath = "/Demo/AllDBInfoALP_Prod.xls"; // Fixed file path
+  const downloadFileName = "AllDBInfoALP_Prod.xls"; // The name the client will see
 
   ftpClient.on("ready", () => {
-    ftpClient.get(fileName ? fileName : "books.csv", (err, stream) => {
+    console.log("ftp fetching");
+
+    ftpClient.get(filePath, (err, stream) => {
       if (err) {
         res.status(500).send("Error retrieving file");
         ftpClient.end();
         return;
       }
+
+      // Set the headers to prompt the client to download the file
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${downloadFileName}`
+      );
+      res.setHeader("Content-Type", "application/vnd.ms-excel");
 
       // Stream the file directly to the response
       stream.pipe(res);
@@ -23,7 +33,7 @@ router.get("/download", (req, res) => {
       });
 
       // Handle errors during streaming
-      stream.on("error", (streamErr : any) => {
+      stream.on("error", (streamErr) => {
         res.status(500).send("Error streaming file");
         ftpClient.end();
       });
@@ -35,6 +45,8 @@ router.get("/download", (req, res) => {
     host: "138-128-244-188.cloud-xip.com",
     user: "alphabroder",
     password: "alpha123",
+    secure: true, // This enables TLS/SSL
+    secureOptions: { rejectUnauthorized: false }, // This is necessary if the server uses a self-signed certificate
   });
 });
 
